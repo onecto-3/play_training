ce79e1cb404ff79913551f809b3f115101626263
onecto-3 <akisei1997@gmail.com>
2020-07-29 12:25:43 +0900

    commit

--- app/controllers/PlusController.scala
+++ app/controllers/PlusController.scala
@@ -7,9 +7,16 @@ import play.api.mvc.Action
 import play.api.mvc.AnyContent
 import play.api.mvc.ControllerComponents
 import play.api.mvc.Request
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 @Singleton
 class PlusController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  
+  implicit val rds = (
+    (__ \ 'name).read[Long] and
+    (__ \ 'age).read[Long]
+  ) tupled
 
   def get(a: Option[Int], b: Option[Int]) =
     Action { implicit request: Request[AnyContent] =>
@@ -20,5 +27,21 @@ class PlusController @Inject()(cc: ControllerComponents) extends AbstractControl
           (a.get+b.get).toString
       }
   }
+
+  def post() = Action(parse.json) { request =>
+    val a:JsResult[Long] = request.body.validate[Long]((__ \ 'a).read[Long])
+    val b:JsResult[Long] = request.body.validate[Long]((__ \ 'b).read[Long])
+
+    (a , b) match{
+      case (x: JsSuccess[Long] , y: JsSuccess[Long]) => Ok(
+        Json.toJson(
+          Map(
+            "sum"->Json.toJson(x.get + y.get)
+          )
+        )
+      )
+      case (_,_) => BadRequest("Error")
+    }
+  }
 }
-       
+
